92c7bde9a6b6f4920d854d925a4f31b2bb0f09b3
Federico <me@fregante.com>
2021-06-01 15:48:27 +0700

    Tests
    Serve /test/demo-page/ manually on localhost
    
    Example:
    npx browser-sync start --server test/demo-page/

--- test/demo-extension/background.js
+++ test/demo-extension/background.js
@@ -2,8 +2,15 @@ import '../../index.ts'; // `content-scripts-register-polyfill`
 
 console.log('Background loaded');
 
-chrome.contentScripts.register({
+(window.browser || chrome).contentScripts.register({
 	matches: ['https://www.google.com/*'],
+	js: [{file: 'dynamic.js'}],
+	css: [{file: 'dynamic.css'}]
+}).then(console.log, console.error);
+
+(window.browser || chrome).contentScripts.register({
+	matches: ['http://localhost/'],
+	allFrames: true,
 	js: [{file: 'dynamic.js'}],
 	css: [{file: 'dynamic.css'}]
 }).then(console.log, console.error);
