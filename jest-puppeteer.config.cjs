2837ce47492c5fdba7a0f9927be5731ffb15a132
Federico <me@fregante.com>
2021-06-19 14:02:43 +0700

    mujo-code/puppeteer-headful with path

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
 	launch: {
 		headless: false,
+		executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
 		args: [
 			'--no-sandbox',
 			'--disable-extensions-except=' + path.resolve(__dirname, 'test/dist'),
