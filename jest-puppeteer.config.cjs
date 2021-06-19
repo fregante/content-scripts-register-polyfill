643c7ae4daca73dafc8d9ca715df2688383804d4
Federico <me@fregante.com>
2021-06-19 14:00:09 +0700

    sandbox only

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,14 +1,12 @@
 /* eslint-disable unicorn/prefer-module */
 const path = require('path');
 
-const extension = path.resolve(__dirname, 'test/dist');
-
 module.exports = {
 	launch: {
 		headless: false,
 		args: [
-			`--disable-extensions-except=${extension}`,
-			`--load-extension=s${extension}`,
+			'--no-sandbox',
+			'--disable-extensions-except=' + path.resolve(__dirname, 'test/dist'),
 			'--window-size=400,800'
 		]
 	}
