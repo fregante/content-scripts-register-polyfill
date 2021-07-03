2c644037bb8f92a71214d47c7f63340ba6aa39d7
Federico <me@fregante.com>
2021-07-03 23:20:11 +0700

    Add comment about the extension in the TS file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 /// <reference path="./globals.d.ts" />
+// The .js extension is required to create ESM-compatible file
 import register from './ponyfill.js';
 
 if (typeof chrome === 'object' && !chrome.contentScripts) {
