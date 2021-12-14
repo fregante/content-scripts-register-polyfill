7b364d5d80b82cae0179d0d74d02ec002bfe938e
Federico Brigante <me@fregante.com>
2021-12-14 19:38:37 +0700

    Extract injection code to share the same logic everywhere (#59)

--- ponyfill.ts
+++ ponyfill.ts
@@ -1,4 +1,4 @@
-import {executeFunction} from 'webext-content-scripts';
+import {executeFunction, executeScript, insertCSS} from 'webext-content-scripts';
 import chromeP from 'webext-polyfill-kinda';
 import {patternToRegex} from 'webext-patterns';
 
@@ -10,17 +10,12 @@ interface Target {
 	frameId: number;
 }
 
-const gotScripting = typeof chrome === 'object' && 'scripting' in chrome;
 const gotNavigation = typeof chrome === 'object' && 'webNavigation' in chrome;
 
 async function isOriginPermitted(url: string): Promise<boolean> {
 	return chromeP.permissions.contains({
 		origins: [new URL(url).origin + '/*'],
 	});
-}
-
-function arrayOrUndefined<X>(value?: X): [X] | undefined {
-	return typeof value === 'undefined' ? undefined : [value];
 }
 
 async function wasPreviouslyLoaded(
@@ -73,59 +68,21 @@ export default async function registerContentScript(
 			return;
 		}
 
-		for (const content of css) {
-			if (gotScripting) {
-				void chrome.scripting.insertCSS({
-					target: {
-						tabId,
-						frameIds: arrayOrUndefined(frameId),
-						allFrames,
-					},
-					files: 'file' in content ? [content.file] : undefined,
-					css: 'code' in content ? content.code : undefined,
-				});
-			} else {
-				void chromeP.tabs.insertCSS(tabId, {
-					...content,
-					matchAboutBlank,
-					allFrames,
-					frameId,
-					runAt: runAt ?? 'document_start', // CSS should prefer `document_start` when unspecified
-				});
-			}
-		}
+		insertCSS({
+			tabId,
+			frameId,
+			files: css,
+			matchAboutBlank,
+			runAt,
+		});
 
-		let lastInjection: Promise<unknown> | undefined;
-		for (const content of js) {
-			if (gotScripting) {
-				if ('code' in content) {
-					throw new Error('chrome.scripting does not support injecting strings of `code`');
-				}
-
-				void chrome.scripting.executeScript({
-					target: {
-						tabId,
-						frameIds: arrayOrUndefined(frameId),
-						allFrames,
-					},
-					files: [content.file],
-				});
-			} else {
-				// Files are executed in order, but code isn’t, so it must wait the last script #31
-				if ('code' in content) {
-					// eslint-disable-next-line no-await-in-loop -- On purpose, to serialize injection
-					await lastInjection;
-				}
-
-				lastInjection = chromeP.tabs.executeScript(tabId, {
-					...content,
-					matchAboutBlank,
-					allFrames,
-					frameId,
-					runAt,
-				});
-			}
-		}
+		await executeScript({
+			tabId,
+			frameId,
+			files: js,
+			matchAboutBlank,
+			runAt,
+		});
 	};
 
 	const tabListener = async (
